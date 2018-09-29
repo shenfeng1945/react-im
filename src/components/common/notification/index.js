5edbd992036320ce27314f324b62acae9a0706d1
shenfeng1945 <823902826@qq.com>
2018-09-29 14:27:50 +0800

    封装消息提示框

---
+++ src/components/common/notification/index.js
@@ -0,0 +1,14 @@
+export const createNotifi = (text= '',type = 'success') => {
+   const dom = document.createElement('div')
+   dom.classList = 'notification' + ' ' +type
+   dom.innerHTML = `
+     <svg class="icon-16">
+       <use xlink:href=#icon-${type}></use>
+     </svg>
+     <span style="margin-left:5px">${text}</span>
+   `
+   document.body.appendChild(dom)
+   setTimeout(()=>{
+     document.body.removeChild(dom)
+   },1000)
+}
