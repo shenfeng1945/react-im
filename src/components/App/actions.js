f9d1505c9936adcb12147fbeee5d543a9dc0a930
shenfeng1945 <823902826@qq.com>
2018-10-05 16:36:54 +0800

    收到别人发的消息的回调,修改msgList

--- src/components/App/actions.js
+++ src/components/App/actions.js
@@ -1,7 +1,14 @@
 import {getFriendList} from '../friends/actions'
 import eventEmitter from '../../utils/event'
-
+import {getMessage} from '../chatPanel/actions'
+import {changeRostersWithMsg} from '../friends/actions'
 const WebIM = window.WebIM
+const handleMessage = (message) => {
+    return dispatch => {
+       dispatch(getMessage(message))
+       dispatch(changeRostersWithMsg(message))
+    }
+}
 export const initSDK = () => {
     return dispatch => {
         WebIM.conn.listen({
@@ -16,7 +23,9 @@ export const initSDK = () => {
                 console.log('onPre')
                 eventEmitter.emit('presence', message)
             },
-            onTextMessage: () => {
+            onTextMessage: (message) => {
+                dispatch(handleMessage(message));
+                console.log('i get success message')
             },
         })
     }
