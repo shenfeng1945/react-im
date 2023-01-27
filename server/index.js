2a3fad27e8a93fa1c38d2043b7da61cd829e9c0e
shenfeng1945 <823902826@qq.com>
2023-01-27 14:45:10 +0800

    更新mongodb连接

--- server/index.js
+++ server/index.js
@@ -1,35 +1,41 @@
-const express = require('express')
-const users = require('./routes/users')
-// const avatar = require('./routes/avatar')
-const cors = require('cors')
-const bodyParser = require('body-parser') 
-const mongoose = require('mongoose')
-const https = require('https')
-const fs = require('fs')
-const app = express()
+const express = require("express");
+const users = require("./routes/users");
+const avatar = require("./routes/avatar");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const https = require("https");
+const fs = require("fs");
+const app = express();
 
-mongoose.connect('mongodb://shenfeng1945:shenfeng1945@ds239911.mlab.com:39911/reac-im',(error)=>{
-    if(error){
-        console.log(error)
-    }else{
-        console.log('mongodb has been connected')
+mongoose.set("strictQuery", true);
+
+mongoose.connect(
+  // "mongodb://shenfeng1945:shenfeng1945@ds239911.mlab.com:39911/reac-im",
+  "mongodb+srv://admin:Ev0ZAqJhfym5i4Y3@cluster0.abmio.mongodb.net/?retryWrites=true&w=majority",
+  { dbName: "react-im" },
+  (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("mongodb has been connected");
     }
-})
+  }
+);
 
-app.use(bodyParser.json())
-app.use(cors())
-// app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(cors());
+//app.use(express.static("public"));
 
-app.use('/api/users',users)
-// app.use('/public/uploadImg',avatar)
+app.use("/api/users", users);
+//app.use("/public/uploadImg", avatar);
 
-const key = fs.readFileSync('/usr/local/nginx/conf/2_shenfeng1945.cn.key','utf8');
-var cert = fs.readFileSync('/usr/local/nginx/conf/1_shenfeng1945.cn_bundle.crt','utf8');
+const key = fs.readFileSync("/etc/ssl/shenfeng1945.key", "utf8");
+var cert = fs.readFileSync("/etc/ssl/shenfeng1945.crt", "utf8");
 var options = {
-    key: key,
-    cert: cert
+  key: key,
+  cert: cert,
 };
 
-// app.listen(6060,()=>console.log('localhost:6060'))
-https.createServer(options,app).listen(6061);
-
+// app.listen(6061, () => console.log("localhost:6061"));
+https.createServer(options, app).listen(6061);
